ebd9271edc99724803bbe71fd1bca25b1d7bdcc6
Sean Murphy <sean@iamseanmurphy.com>
2026-03-26 10:30:51 +0000

    1.2.4

--- genesis-simple-share/plugin.php
+++ genesis-simple-share/plugin.php
@@ -3,7 +3,7 @@
  * Plugin Name: Genesis Simple Share
  * Plugin URI: https://wordpress.org/plugins/genesis-simple-share/
  * Description: A simple sharing plugin using the Share script.
- * Version: 1.2.3
+ * Version: 1.2.4
  * Author: OsomPress
  * Author URI: https://www.osompress.com
  *
@@ -17,20 +17,26 @@ if ( ! defined( 'ABSPATH' ) ) {
 	die( esc_html__( 'Sorry, you are not allowed to access this page directly.', 'genesis-simple-share' ) );
 }
 
-define( 'GENESIS_SIMPLE_SHARE_VERSION', '1.2.3' );
+define( 'GENESIS_SIMPLE_SHARE_VERSION', '1.2.4' );
 define( 'GENESIS_SIMPLE_SHARE_PATH', plugin_dir_path( __FILE__ ) );
 define( 'GENESIS_SIMPLE_SHARE_INC', plugin_dir_path( __FILE__ ) . '/includes/' );
 define( 'GENESIS_SIMPLE_SHARE_URL', plugins_url( '', __FILE__ ) );
 
-add_action( 'genesis_init', 'genesis_simple_share_init', 99 );
+add_action( 'init', 'genesis_simple_share_translations_init', 1 );
+add_action( 'init', 'genesis_simple_share_init', 99 );
 /**
  * Loads plugin required files.
  *
  * @since 0.1.0
- * changed on version 1.2.3
+ * changed on version 1.2.4
  * @uses GENESIS_SIMPLE_SHARE_INC
  */
 function genesis_simple_share_init() {
+
+	// Genesis Simple Share depends on Genesis framework functions.
+	if ( ! function_exists( 'genesis_get_option' ) ) {
+		return;
+	}
 
 	if ( is_admin() && class_exists( 'Genesis_Admin_Boxes' ) ) {
 		require_once GENESIS_SIMPLE_SHARE_INC . 'class-genesis-simple-share-boxes.php';
@@ -40,14 +46,17 @@ function genesis_simple_share_init() {
 	}
 }
 
-add_action( 'init', 'genesis_simple_share_translations_init', 99 );
 /**
  * Loads plugin text domain. Uses init instead of genesis_init to avoid loading too early.
  *
  * @since 0.1.0
- * changed on version 1.2.3
+ * changed on version 1.2.4
  * @uses GENESIS_SIMPLE_SHARE_PATH
  */
 function genesis_simple_share_translations_init() {
-	load_plugin_textdomain( 'genesis-simple-share', false, GENESIS_SIMPLE_SHARE_PATH . '/languages/' );
+	load_plugin_textdomain(
+		'genesis-simple-share',
+		false,
+		dirname( plugin_basename( __FILE__ ) ) . '/languages'
+	);
 }
--- genesis-simple-share/readme.txt
+++ genesis-simple-share/readme.txt
@@ -2,8 +2,8 @@
 Contributors: nathanrice, studiopress, wpmuguru, nick_thegeek, bgardner, marksabbath, osompress, esther_sola, nahuai
 Tags: genesis, share, share buttons, social
 Requires at least: 3.7
-Tested up to: 6.9
-Stable tag: 1.2.3
+Tested up to: 7.0
+Stable tag: 1.2.4
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
@@ -30,6 +30,9 @@ https://github.com/copyblogger/genesis-simple-share/wiki/Usage-Tips
 
 
 == Changelog ==
+
+= 1.2.4 =
+* Iteration on multisite compatibility and translation loading improvements.
 
 = 1.2.3 =
 * Improve multisite compatibility.
